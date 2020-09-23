396326002e631200c0dd76ace00b836cbbffff46
LJG070 <dlwlsrn94@naver.com>
2020-09-23 19:59:58 +0900

    [master] 프로젝트 초기 세팅

---
+++ src/main/java/com/kookmin/lyl/LylApplication.java
@@ -0,0 +1,13 @@
+package com.kookmin.lyl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LylApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LylApplication.class, args);
+    }
+
+}
