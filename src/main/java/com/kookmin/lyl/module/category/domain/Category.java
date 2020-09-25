7885c00e74c2465418dd1883f7629635270e9e16
LJG070 <dlwlsrn94@naver.com>
2020-09-25 21:34:07 +0900

    [feature/category] category 엔티티 추가

--- src/main/java/com/kookmin/lyl/module/category/domain/Category.java
+++ src/main/java/com/kookmin/lyl/module/category/domain/Category.java
@@ -1,4 +1,45 @@
 package com.kookmin.lyl.module.category.domain;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name="CATEGORY")
 public class Category {
+    private final static Long DEFAULT_SORT_ORDER = 0L;
+
+    @Id @GeneratedValue
+    @Column(name="CATEGORY_ID")
+    private Long id;
+
+    @Column(name="NAME")
+    private String name;
+
+    @Column(name="SORT_ORDER")
+    private Long sortOrder;
+
+    @Builder
+    public Category(String name) {
+        this.name = name;
+        this.sortOrder = DEFAULT_SORT_ORDER;
+    }
+
+    public void swapSortOrder(Category targetCategory) {
+        long originSortOrder = this.sortOrder;
+        this.sortOrder = targetCategory.sortOrder;
+        targetCategory.sortOrder = originSortOrder;
+    }
+
+    public void initSortOrder(Category category) {
+        this.sortOrder = category.getSortOrder() + 1L;
+    }
+
+    public void editName(String name) {
+        this.name = name;
+    }
 }
