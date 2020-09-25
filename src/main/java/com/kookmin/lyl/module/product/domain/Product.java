3f34ae657bbccbcaa1737146e2e2ca33fd2be5d9
LJG070 <dlwlsrn94@naver.com>
2020-09-25 22:50:41 +0900

    [feature/Product] Product 엔티티 추가, 관련 클래스들 추가 구현 필요

--- src/main/java/com/kookmin/lyl/module/product/domain/Product.java
+++ src/main/java/com/kookmin/lyl/module/product/domain/Product.java
@@ -1,4 +1,45 @@
 package com.kookmin.lyl.module.product.domain;
 
+import com.kookmin.lyl.module.category.domain.Category;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name="PRODUCT")
 public class Product {
+    @Id @GeneratedValue
+    @Column(name="PRODUCT_ID")
+    private Long productNumber;
+
+    @Column(name="NAME")
+    private String name;
+
+    @Column(name="PRICE")
+    private Integer price;
+
+    @Column(name="MANUFACTURER")
+    private String manufacturer;
+
+    @Column(name="ORIGIN")
+    private String origin;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name="STATUS")
+    private ProductStatus status;
+
+    @ManyToOne
+    @JoinColumn(name="CATEGORY_ID")
+    private Category category;
+
+    @ManyToOne
+    @JoinColumn(name="SHOP_ID")
+    private Shop shop;
+
+    @OneToMany(mappedBy = "product")
+    private List<ProductOption> productOptions = new ArrayList<ProductOption>();
 }
