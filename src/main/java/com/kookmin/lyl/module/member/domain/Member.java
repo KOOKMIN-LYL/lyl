f9f7454429b550c8ba84c998c269f962af8f61cd
Junmannn <junmannnyoon@gmnail.com>
2020-09-27 19:57:53 +0900

    feature/member 부 domain - Member, MemberStatus, MemberType 구현

--- src/main/java/com/kookmin/lyl/module/member/domain/Member.java
+++ src/main/java/com/kookmin/lyl/module/member/domain/Member.java
@@ -8,10 +8,10 @@ import javax.persistence.*;
 
 @Entity
 @Getter
+@NoArgsConstructor
 @Table(name="Member")
 public class Member {
-    private final static Long DEFAULT_SORT_ORDER = 0L;
-
+    // usn은 자동생성으로
     @Id @GeneratedValue
     @Column(name="USN")
     private Long usn;
@@ -19,48 +19,47 @@ public class Member {
     @Column(name="ID")
     private String id;
 
-    @Column(name="SORT_ORDER")
-    private Long sortOrder;
-
-    @Column(name="name")
+    @Column(name="NAME")
     private String name;
 
-    @Column(name="address")
+    @Column(name="ADDRESS")
     private String address;
 
+    @Column(name="PASSWORD")
+    private String password;
 
 //    @Column(name="zipCode")
 //    private int zipCode;
 //    짚코드는 빼자
 
-    @Column(name="phone")
+    @Column(name="PHONE")
     private String phone;
 
-    @Column(name="email")
+    @Column(name="EMAIL")
     private String email;
 
-    @Column(name="status")
-    private Enum status;
+    @Enumerated(EnumType.STRING)
+    @Column(name="STATUS")
+    private MemberStatus status;
 
-    @Column(name="memberType")
-    private Enum memberType;
-
-
+    @Enumerated(EnumType.STRING)
+    private MemberType memberType;
 
     @Builder
-    public Member(String name) {
+    public Member(String name, long usn, String id, String password, String address, String phone, String email) {
         this.name = name;
-        this.sortOrder = DEFAULT_SORT_ORDER;
-    }
-
-    public void swapSortOrder(Member targetMember) {
-        long originSortOrder = this.sortOrder;
-        this.sortOrder = targetMember.sortOrder;
-        targetMember.sortOrder = originSortOrder;
+        this.id = id;
+        this.address = address;
+        this.usn = usn;
+        this.password = password;
+        this.phone = phone;
+        this.email = email;
+        this.status = MemberStatus.ACTIVE;
+        this.memberType = MemberType.USER;
     }
 
     public void editName(String name) {
-
         this.name = name;
     }
+
 }
---
+++ src/main/java/com/kookmin/lyl/module/member/domain/MemberStatus.java
@@ -0,0 +1,7 @@
+package com.kookmin.lyl.module.member.domain;
+
+public enum MemberStatus {
+    // 활성 회원인지 휴면 회원인지 구분을 위한 member 의 status
+    ACTIVE,
+    EXPIRED
+}
