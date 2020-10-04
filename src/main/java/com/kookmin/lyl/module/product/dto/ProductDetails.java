ae6dd294fdfeac4f9c09e33da2e89fbfc82950a6
LJG070 <dlwlsrn94@naver.com>
2020-10-04 11:09:35 +0900

    [feature/product] ProductDetails 쿼리dsl 조회용 @QueryProjection 추가

--- src/main/java/com/kookmin/lyl/module/product/dto/ProductDetails.java
+++ src/main/java/com/kookmin/lyl/module/product/dto/ProductDetails.java
@@ -2,6 +2,7 @@ package com.kookmin.lyl.module.product.dto;
 
 import com.kookmin.lyl.module.product.domain.Product;
 import com.kookmin.lyl.module.product.domain.ProductStatus;
+import com.querydsl.core.annotations.QueryProjection;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -16,6 +17,21 @@ public class ProductDetails {
     private ProductStatus status;
     private Long categoryId;
     private Long shopId;
+
+    @QueryProjection
+    public ProductDetails(Long productNumber, String name, Integer price, String manufacturer, String origin,
+                          ProductStatus status, Long categoryId, Long shopId) {
+        this.productNumber = productNumber;
+        this.name = name;
+        this.price = price;
+        this.manufacturer = manufacturer;
+        this.origin = origin;
+        this.status = status;
+        this.categoryId = categoryId;
+        this.shopId = shopId;
+    }
+
+
 
     //TODO:: 지연로딩때문에 페치 조인 전략으로 바꿀 필요가 있음
     public ProductDetails(Product product) {
