b29b36d36d9c070641393f4901bfd61318008b43
LJG070 <dlwlsrn94@naver.com>
2020-10-13 18:08:01 +0900

    [feature/product] product컨트롤러에 더미데이터 및 카테고리별 상품 조회 메소드 추가

--- src/main/java/com/kookmin/lyl/LylApplication.java
+++ src/main/java/com/kookmin/lyl/LylApplication.java
@@ -2,12 +2,12 @@ package com.kookmin.lyl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class LylApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LylApplication.class, args);
     }
-
 }
