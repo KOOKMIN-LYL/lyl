d7f24066063143d02725327ccf138df2aed64323
LJG070 <dlwlsrn94@naver.com>
2020-10-27 14:56:35 +0900

    [feature/webSecurity] MemberController 추가

--- src/main/java/com/kookmin/lyl/web/MemberController.java
+++ src/main/java/com/kookmin/lyl/web/MemberController.java
@@ -1,13 +1,29 @@
 package com.kookmin.lyl.web;
 
+import com.kookmin.lyl.module.member.dto.MemberCreateInfo;
 import com.kookmin.lyl.module.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
+import java.security.Principal;
 
 @RestController
 @RequiredArgsConstructor
 public class MemberController {
     private final MemberService memberService;
 
-
+    @PostConstruct
+    public void setUpMember() {
+        MemberCreateInfo memberCreateInfo = new MemberCreateInfo();
+        memberCreateInfo.setId("user");
+        memberCreateInfo.setPassword("123");
+        memberCreateInfo.setAddress("주소");
+        memberCreateInfo.setName("유저1");
+        memberCreateInfo.setEmail("dlwlsrn9411@kookmin.ac.kr");
+        memberCreateInfo.setPhone("00-00-00");
+        memberService.join(memberCreateInfo);
+    }
 }
