1b7dd70186d3348369b897c37475dc8b0a9b92b4
LJG070 <dlwlsrn94@naver.com>
2020-10-27 15:06:24 +0900

    [feature/webSecurity] OrderController addProductToCart 메소드 추가

--- src/main/java/com/kookmin/lyl/web/OrderController.java
+++ src/main/java/com/kookmin/lyl/web/OrderController.java
@@ -1,13 +1,24 @@
 package com.kookmin.lyl.web;
 
+import com.kookmin.lyl.module.order.dto.OrderProductInfo;
 import com.kookmin.lyl.module.order.service.OrderService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 @RestController
 @RequiredArgsConstructor
 public class OrderController {
     private final OrderService orderService;
+
+    @PostMapping(value = "/cart/product")
+    public String addProductToCart(OrderProductInfo orderProductInfo, Principal principal) {
+        orderService.addCart(principal.getName(), orderProductInfo);
+
+        return "ok";
+    }
 
 
 }
