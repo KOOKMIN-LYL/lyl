640e83ca5846f41817332632b972458e8428ae9d
LJG070 <dlwlsrn94@naver.com>
2020-10-29 10:59:42 +0900

    [feature/webSecurity] CORS 이슈 해결 및 커스텀 필터 추가와 response, request에 헤더 값 추가

--- src/main/java/com/kookmin/lyl/infra/config/SecurityConfig.java
+++ src/main/java/com/kookmin/lyl/infra/config/SecurityConfig.java
@@ -1,41 +1,72 @@
 package com.kookmin.lyl.infra.config;
 
+import com.google.common.collect.ImmutableList;
 import com.kookmin.lyl.module.member.service.MemberService;
-import com.kookmin.lyl.web.handler.LoginSuccessHandler;
+import com.kookmin.lyl.web.filter.CustomResponseHeaderFilter;
 import com.kookmin.lyl.web.resource.WebPath;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationFilter;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.context.SecurityContextPersistenceFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
     private final MemberService memberService;
-
+    private final CustomResponseHeaderFilter customResponseHeaderFilter;
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.addFilterBefore(customResponseHeaderFilter, SecurityContextPersistenceFilter.class);
+
+        http.csrf().disable();
+
+        http.formLogin()
+                .successHandler(successHandler())
+                .failureHandler(failureHandler())
+                .permitAll();
+
         http.authorizeRequests()
                 .anyRequest().authenticated();
 
-        http.formLogin()
-        .successHandler(authenticationSuccessHandler())
-        .usernameParameter("username")
-        .passwordParameter("password");
+        http.exceptionHandling()
+               .accessDeniedHandler(accessDeniedHandler());
 
         http.httpBasic();
-        http.csrf()
-                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        registry.addMapping("/**")
+                .allowedMethods("*")
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .allowedHeaders("X-XSRF-TOKEN")
+                .allowedHeaders("Access-Control-Allow-Origin");
+
     }
 
     @Override
@@ -44,20 +75,41 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.addAllowedOrigin("/**");
-        configuration.addAllowedHeader("*");
-        configuration.addAllowedMethod("*");
-        configuration.setAllowCredentials(true);
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
+    public AuthenticationSuccessHandler successHandler() {
+        return new AuthenticationSuccessHandler() {
+            @Override
+            public void onAuthenticationSuccess(HttpServletRequest httpServletRequest,
+                                                HttpServletResponse httpServletResponse,
+                                                Authentication authentication) throws IOException, ServletException {
+                httpServletResponse.getWriter().append("OK");
+                httpServletResponse.setStatus(200);
+            }
+        };
     }
 
     @Bean
-    public AuthenticationSuccessHandler authenticationSuccessHandler() {
-        return new LoginSuccessHandler();
+    public AuthenticationFailureHandler failureHandler() {
+        return new AuthenticationFailureHandler() {
+            @Override
+            public void onAuthenticationFailure(HttpServletRequest httpServletRequest,
+                                                HttpServletResponse httpServletResponse,
+                                                AuthenticationException e) throws IOException, ServletException {
+                httpServletResponse.getWriter().append("Authentication failure");
+                httpServletResponse.setStatus(401);
+            }
+        };
+    }
+
+    @Bean
+    public AccessDeniedHandler accessDeniedHandler() {
+        return new AccessDeniedHandler() {
+            @Override
+            public void handle(HttpServletRequest httpServletRequest,
+                               HttpServletResponse httpServletResponse,
+                               AccessDeniedException e) throws IOException, ServletException {
+                httpServletResponse.getWriter().append("Access denied");
+                httpServletResponse.setStatus(403);
+            }
+        };
     }
 }
