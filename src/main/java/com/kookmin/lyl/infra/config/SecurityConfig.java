2b4b6b648ad271e80e6923d273b9e9a7050887ca
LJG070 <dlwlsrn94@naver.com>
2020-10-30 17:28:13 +0900

    [feature/webSecurity] JWT 인증 방식으로 변경

--- src/main/java/com/kookmin/lyl/infra/config/SecurityConfig.java
+++ src/main/java/com/kookmin/lyl/infra/config/SecurityConfig.java
@@ -1,8 +1,10 @@
 package com.kookmin.lyl.infra.config;
 
 import com.google.common.collect.ImmutableList;
+import com.kookmin.lyl.infra.support.JwtTokenProvider;
 import com.kookmin.lyl.module.member.service.MemberService;
 import com.kookmin.lyl.web.filter.CustomResponseHeaderFilter;
+import com.kookmin.lyl.web.filter.JwtAuthenticationFilter;
 import com.kookmin.lyl.web.resource.WebPath;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
@@ -13,12 +15,16 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationFilter;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.context.SecurityContextPersistenceFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
@@ -34,27 +40,21 @@ import java.io.IOException;
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
-    private final MemberService memberService;
     private final CustomResponseHeaderFilter customResponseHeaderFilter;
+    private final JwtTokenProvider jwtTokenProvider;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.addFilterBefore(customResponseHeaderFilter, SecurityContextPersistenceFilter.class);
-
+        //http.addFilterBefore(customResponseHeaderFilter, SecurityContextPersistenceFilter.class);
+        http.addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider),
+                UsernamePasswordAuthenticationFilter.class);
+        http.httpBasic().disable();
         http.csrf().disable();
-
-        http.formLogin()
-                .successHandler(successHandler())
-                .failureHandler(failureHandler())
-                .permitAll();
-
+        http.sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.authorizeRequests()
+                .mvcMatchers("/category", "/login", "/member/join").permitAll()
                 .anyRequest().authenticated();
-
-        http.exceptionHandling()
-               .accessDeniedHandler(accessDeniedHandler());
-
-        http.httpBasic();
-
     }
 
     @Override
@@ -64,7 +64,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter implements WebM
                 .allowedMethods("*")
                 .allowedOrigins("*")
                 .allowedHeaders("*")
-                .allowedHeaders("X-XSRF-TOKEN")
                 .allowedHeaders("Access-Control-Allow-Origin");
 
     }
@@ -72,44 +71,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter implements WebM
     @Override
     public void configure(WebSecurity web) throws Exception {
         web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
-    }
-
-    @Bean
-    public AuthenticationSuccessHandler successHandler() {
-        return new AuthenticationSuccessHandler() {
-            @Override
-            public void onAuthenticationSuccess(HttpServletRequest httpServletRequest,
-                                                HttpServletResponse httpServletResponse,
-                                                Authentication authentication) throws IOException, ServletException {
-                httpServletResponse.getWriter().append("OK");
-                httpServletResponse.setStatus(200);
-            }
-        };
-    }
-
-    @Bean
-    public AuthenticationFailureHandler failureHandler() {
-        return new AuthenticationFailureHandler() {
-            @Override
-            public void onAuthenticationFailure(HttpServletRequest httpServletRequest,
-                                                HttpServletResponse httpServletResponse,
-                                                AuthenticationException e) throws IOException, ServletException {
-                httpServletResponse.getWriter().append("Authentication failure");
-                httpServletResponse.setStatus(401);
-            }
-        };
-    }
-
-    @Bean
-    public AccessDeniedHandler accessDeniedHandler() {
-        return new AccessDeniedHandler() {
-            @Override
-            public void handle(HttpServletRequest httpServletRequest,
-                               HttpServletResponse httpServletResponse,
-                               AccessDeniedException e) throws IOException, ServletException {
-                httpServletResponse.getWriter().append("Access denied");
-                httpServletResponse.setStatus(403);
-            }
-        };
     }
 }
